eb82c3429e0ac0ee1d9bc0c888e642c851077c9a
Hugo Santos <Hugo.Santos@PE06VTQT.vilt.local>
2022-01-25 10:35:06 -0300

    Exercices

---
+++ src/Class/Funcionario.java
@@ -0,0 +1,46 @@
+package Class;
+
+public class Funcionario {
+	private int id;
+    private String name;
+    private double salary;
+
+    public Funcionario(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return id + ", " + name + ", " + salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percent) {
+        this.salary += salary * percent/100;
+    }
+}
